f848c0e8f0226a6a4e51d7186cf4ec20b92d3434
Alexandre Rodrigues <alxrds@hotmail.com>
2023-03-14 11:42:31 -0300

    concluindo projeto

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     tableBook = $("#book_table").DataTable({  
         ajax:{            
-            url: "http://localhost:8089/book/list/", 
+            url: "book/list/", 
             method: "GET", 
             dataSrc:""
         },
@@ -12,13 +12,92 @@ $(document).ready(function () {
             {data: "editora"},
             {data: "genero"},
             {data: "paginas"},
-            {defaultContent: "<div class='text-center'><div class='btn-group'><button class='btn btn-primary btn-sm btnEditar'><i class='bi bi-pencil-square'></i></button><button class='btn btn-danger btn-sm btnExcluir'><i class='bi bi-trash-fill'></i></button></div></div>"}
+            {defaultContent: "<div class='text-center'><div class='btn-group'><button class='btn btn-primary btn-sm btnEditar' data-toggle='modal' data-target='#modal_book'><i class='bi bi-pencil-square'></i></button><button class='btn btn-danger btn-sm btnExcluir'><i class='bi bi-trash-fill'></i></button></div></div>"}
         ]
     });
+
+    $('#book_form').submit(function(e){                         
+        e.preventDefault();
+        nome = $.trim($('#nome').val());    
+        escritor = $.trim($('#escritor').val());
+        editora = $.trim($('#editora').val());    
+        genero = $.trim($('#genero').val());    
+        paginas = $.trim($('#paginas').val());
+        if(id != null){
+            $.ajax({
+                url: "book/edit",
+                type: "POST",
+                datatype:"json",    
+                data:  {id:id, nome:nome, escritor:escritor, editora:editora, genero:genero, paginas:paginas},    
+                success: function(data) {
+                    tableBook.ajax.reload(null, false);
+                    if(data == 2){
+                        $('#modal_book').modal('hide');	
+                        Swal.fire (
+                            'Aviso',
+                            'Book Editado com Sucesso',
+                            'success'
+                        );
+                    }
+                }
+            });
+
+        }else{
+
+            $.ajax({
+                url: "book/new",
+                type: "POST",
+                datatype:"json",    
+                data:  {nome:nome, escritor:escritor, editora:editora, genero:genero, paginas:paginas},    
+                success: function(data) {
+                    tableBook.ajax.reload(null, false);
+                    if(data == 1){
+                        $('#modal_book').modal('hide');	
+                        Swal.fire (
+                            'Aviso',
+                            'Book incluído com Sucesso',
+                            'success'
+                        );
+                    }
+                }
+            });
+
+        }
+
+    });
     
+    $(document).on("click", ".btn_new", function(){
+        $("#book_form").trigger("reset");
+        tituloModal = $('#modal_title').text("Add Book");
+        tituloBotao = $('#btn_action').text('Add');
+        id = null;
+        nome = $("#nome").val();
+        escritor = $("#escritor").val();
+        editora = $("#editora").val();
+        genero = $("#genero").val();
+        paginas = $("#paginas").val();
+    });
+
+    $(document).on("click", ".btnEditar", function(){
+        tituloModal = $('#modal_title').text('Edit Book');
+        tituloBotao = $('#btn_action').text('Edit');
+        linha = $(this).closest("tr");  
+        id = parseInt($(this).closest('tr').find('td:eq(0)').text());
+        nome = linha.find('td:eq(1)').text();
+        escritor = linha.find('td:eq(2)').text();
+        editora = linha.find('td:eq(3)').text();
+        genero = linha.find('td:eq(4)').text();
+        paginas = linha.find('td:eq(5)').text();
+        $("#nome").val(nome);
+        $("#escritor").val(escritor);
+        $("#editora").val(editora);
+        $("#genero").val(genero);
+        $("#paginas").val(paginas);
+    });
+
     $(document).on("click", ".btnExcluir", function(){
-        fila = $(this);           
-        id = parseInt($(this).closest('tr').find('td:eq(0)').text()) ;	
+        linha = $(this);           
+        id = parseInt($(this).closest('tr').find('td:eq(0)').text());	
         Swal.fire({
             title: "Aviso",
             text: "Essa ação não podera ser desfeita!",
@@ -33,8 +112,8 @@ $(document).ready(function () {
                 $.ajax({
                     url: "http://localhost:8089/book/remove/" + id,
                     type: "GET",
-                    success: function() {
-                        tableBook.row(fila.parents("tr")).remove().draw();
+                    success: function(){
+                        tableBook.row(linha.parents("tr")).remove().draw();
                         Swal.fire (
                             "Aviso",
                             "Evento Excluído com Sucesso",
@@ -45,6 +124,10 @@ $(document).ready(function () {
             }
         });
     });
+
+
+
+
 });
 
   
--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -16,6 +16,12 @@
             return $view->render();
         }
 
+        public function list()
+        {   $connection = Connection::getInstance();
+            $json = (new Book($connection))->findAll();
+            return json_encode($json, JSON_UNESCAPED_UNICODE);
+        }
+
         public function new()
         {
             $connection = Connection::getInstance();
@@ -31,10 +37,18 @@
             }
         }
 
-        public function list()
-        {   $connection = Connection::getInstance();
-            $json = (new Book($connection))->findAll();
-            return json_encode($json, JSON_UNESCAPED_UNICODE);
+        public function edit($id)
+        {   
+            $data = $_POST;
+            $id = $data['id'];
+            $connection = Connection::getInstance();
+            $book = new Book($connection);
+            $success = $book->update($data);
+            if($success){
+                return 2;
+            }else{
+                return 0;
+            }
         }
 
         public function remove($id)
@@ -48,18 +62,5 @@
                 return 0;
             }
         }
-
-        public function drop($id)
-        {   
-            $data = $_POST;
-            $id = $data['id'];
-            $connection = Connection::getInstance();
-            $book = new Book($connection);
-            $success = $book->update($data);
-            if($success){
-                return 4;
-            }else{
-                return 0;
-            }
-        }
+        
     }
